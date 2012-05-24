2cbf9ed8589970f9eefa8e4777c3ef97332a4d7b
Kyle VanderBeek <kylev@kylev.com>
2012-05-23 18:21:40 -0700

    Essential dice definition, creation, and rolling.

--- src/cj_zdice/models/game.clj
+++ src/cj_zdice/models/game.clj
@@ -1,2 +1,15 @@
 (ns cj-zdice.models.game)
 
+(def dice-kinds [{:kind :green,  :sides (concat (repeat 3 :brain) (repeat 2 :runner) (repeat 1 :bang))}
+                 {:kind :yellow, :sides (concat (repeat 2 :brain) (repeat 2 :runner) (repeat 2 :bang))}
+                 {:kind :red,    :sides (concat (repeat 1 :brain) (repeat 2 :runner) (repeat 3 :bang))}])
+
+(defn dice-of
+  "Return a die of the desired type."
+  [desired]
+  (first (filter #(= desired (% :kind)) dice-kinds)))
+
+(defn dice-roll
+  "Roll the list of dice and return their visible variants."
+  [dice]
+  (map #(hash-map :kind (% :kind) :showing (rand-nth (% :sides))) dice))
