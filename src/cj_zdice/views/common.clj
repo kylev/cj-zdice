1eca7ce61097227edf5c13707e373fb0aff88b52
Kyle VanderBeek <kylev@kylev.com>
2012-05-26 12:51:56 -0700

    Get the basic look working with a flexible layout.

--- src/cj_zdice/views/common.clj
+++ src/cj_zdice/views/common.clj
@@ -7,6 +7,17 @@
   [:ul.nav
     [:li (link-to "/about" "About")]])
 
+(defpartial page-header []
+  [:header
+    [:div.ym-wrapper [:div.ym-wbox
+    [:h1 "Zombie Dice"]]]])
+
+(defpartial page-footer []
+  [:footer
+    [:div.ym-wrapper [:div.ym-wbox
+    [:p "© Kyle V 2012. Zombie Dice is the property of " (link-to "http://www.sjgames.com/" "Steve Jackson Games") "."]]]])
+
+
 (defpartial amazon []
   [:iframe {:src "http://rcm.amazon.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=kylevcom-20&o=1&p=8&l=as4&m=amazon&f=ifr&ref=ss_til&asins=B003IKMR0U"
           :style "width:120px;height:240px;" :scrolling "no" :marginwidth "0" :marginheight "0" :frameborder "0"}])
@@ -15,9 +26,11 @@
   (html5
     [:head
       [:title "Zombie Dice"]
-      (include-css "/css/reset.css")
-      (include-js "/js/jquery-1.7.2.min.js" "/js/backbone-min.js")]
+      [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}] ; YAML mobile viewport optimization
+      (include-css "/css/zd.css")
+      (include-js "/js/jquery-1.7.2.min.js" "/js/backbone-min.js" "/js/zd.js")]
     [:body
-      [:div#wrapper
-        (link-bar)
-        content]]))
+      (page-header)
+      [:div#main [:div.ym-wrapper [:div.ym-wbox
+        [:div.ym-grid [:div.ym-g80.ym-gl content] [:div.ym-g20.ym-gr (amazon)]]]]]
+      (page-footer)]))
--- src/cj_zdice/views/welcome.clj
+++ src/cj_zdice/views/welcome.clj
@@ -6,14 +6,12 @@
 
 (defpage "/" []
   (common/layout
-    [:p "Welcome to cj-zdice"]))
+    [:p "Welcome to cj-zdice.  This is a bunch of text so that things push out a little bit and that I can maybe see how things lay out."]))
 
 (defpage "/about" []
   (common/layout
-    [:h1 "About CJ-Zdice"]
     [:p "I am attempting to learn "
         (link-to "htttp://www.clojure.org/" "Clojure")
         " by implementing the excellent game of "
         (link-to "http://www.sjgames.com/dice/zombiedice/" "Zombie Dice")
-        ". You should really just go buy it."]
-    (common/amazon)))
+        ". You should really just go buy it."]))
