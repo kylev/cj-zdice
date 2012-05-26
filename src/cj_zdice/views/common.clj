d297289d3659f0ad54ab8e65f09baef7b8fc0350
Kyle VanderBeek <kylev@kylev.com>
2012-05-26 13:39:39 -0700

    DR-up YAML common wrapper element.

--- src/cj_zdice/views/common.clj
+++ src/cj_zdice/views/common.clj
@@ -3,24 +3,32 @@
         [hiccup.page :only [include-css include-js html5]]
         [hiccup.element :only [link-to]]))
 
+; YAML helpers
+
+(defpartial ym-wrap [content] [:div.ym-wrapper [:div.ym-wbox content]])
+
 (defpartial link-bar []
   [:ul.nav
     [:li (link-to "/about" "About")]])
 
+; Layout compontents
+
 (defpartial page-header []
   [:header
-    [:div.ym-wrapper [:div.ym-wbox
-    [:h1 "Zombie Dice"]]]])
+    (ym-wrap [:h1 "Zombie Dice"])])
 
 (defpartial page-footer []
   [:footer
-    [:div.ym-wrapper [:div.ym-wbox
-    [:p "© Kyle V 2012. Zombie Dice is the property of " (link-to "http://www.sjgames.com/" "Steve Jackson Games") "."]]]])
+    (ym-wrap 
+      [:p "© Kyle V 2012. Zombie Dice is the property of " (link-to "http://www.sjgames.com/" "Steve Jackson Games") "."])])
 
+; Monetization
 
 (defpartial amazon []
   [:iframe {:src "http://rcm.amazon.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=kylevcom-20&o=1&p=8&l=as4&m=amazon&f=ifr&ref=ss_til&asins=B003IKMR0U"
-          :style "width:120px;height:240px;" :scrolling "no" :marginwidth "0" :marginheight "0" :frameborder "0"}])
+            :style "width:120px;height:240px;" :scrolling "no" :marginwidth "0" :marginheight "0" :frameborder "0"}])
+
+; Core layout
 
 (defpartial layout [& content]
   (html5
@@ -31,6 +39,6 @@
       (include-js "/js/jquery-1.7.2.min.js" "/js/backbone-min.js" "/js/zd.js")]
     [:body
       (page-header)
-      [:div#main [:div.ym-wrapper [:div.ym-wbox
-        [:div.ym-grid [:div.ym-g80.ym-gl content] [:div.ym-g20.ym-gr (amazon)]]]]]
+      [:div#main (ym-wrap
+        [:div.ym-grid [:div.ym-g80.ym-gl content] [:div.ym-g20.ym-gr (amazon)]])]
       (page-footer)]))
