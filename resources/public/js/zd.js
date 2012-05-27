35fe8de8bb1bca466c59420dd0d7c8084ec43edb
Kyle VanderBeek <kylev@kylev.com>
2012-05-27 14:01:14 -0700

    Basic player list controls.

--- resources/public/js/zd.js
+++ resources/public/js/zd.js
@@ -1,1 +1,5 @@
-/* TODO Implement everything */
+function ready_scorer() {
+  $("#playerlist")
+    .accordion({header: "> li > div[class=\"playerline\"]", collapsible: true, active: false})
+    .sortable({axis: "y"});
+};
--- src/cj_zdice/views/score.clj
+++ src/cj_zdice/views/score.clj
@@ -1,9 +1,15 @@
 (ns cj-zdice.views.score
   (:require [cj-zdice.views.common :as common])
-  (:use [noir.core :only [defpartial defpage]]))
+  (:use [noir.core :only [defpartial defpage]]
+        [hiccup.element :only [javascript-tag]]))
+
+(defpartial player-container [pname]
+  [:li [:div.playerline (str pname ": ") [:span.score "0"]]
+       [:div.controls [:button.nexplayer "Next"] [:button.score-plus "+"] [:button.score-minus "-"]]])
 
 (defpage "/score" []
   (common/layout
     [:ul#playerlist
-      [:li "Player 1"]
-      [:li "Player 2"]]))
+      (player-container "First Player")
+      (player-container "Second Player")]
+    (javascript-tag "$(ready_scorer)")))
