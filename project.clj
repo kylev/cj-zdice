96932fe1793462cfeb45e671c10838cb0edd98f5
Kyle VanderBeek <kylev@kylev.com>
2013-11-28 11:16:48 -0800

    Move to clojure 1.5.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject cj-zdice "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
-            :dependencies [[org.clojure/clojure "1.4.0"]
+            :dependencies [[org.clojure/clojure "1.5.1"]
                            [noir "1.3.0-beta7"]]
             :main cj-zdice.server)
 
