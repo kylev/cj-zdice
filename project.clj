c43299eb06d10edc8be190d3dc7078644811bf6f
Kyle VanderBeek <kylev@kylev.com>
2014-05-12 12:19:01 -0700

    Start converting parts to om

--- project.clj
+++ project.clj
@@ -1,6 +1,22 @@
 (defproject cj-zdice "0.1.0-SNAPSHOT"
-            :description "FIXME: write this!"
-            :dependencies [[org.clojure/clojure "1.5.1"]
-                           [noir "1.3.0-beta7"]]
-            :main cj-zdice.server)
+  :description "FIXME: write this!"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2202"]
+                 [ring/ring "1.2.2"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 [om "0.6.2"]]
+  :main cj-zdice.server
 
+  :plugins [[lein-cljsbuild "1.0.3"]]
+
+  :source-paths ["src/clj" "src/cljs"]
+  :resource-paths ["resources"]
+
+  :cljsbuild {
+    :builds [{:id "dev"
+              :source-paths ["src/clj" "src/cljs"]
+              :compiler {
+                :output-to "resources/public/js/main.js"
+                :output-dir "resources/public/js/out"
+                :optimizations :none
+                :source-map true}}]})
---
+++ src/cljs/cj_zdice/core.cljs
@@ -0,0 +1,1 @@
+(ns cj-zdice.core)
