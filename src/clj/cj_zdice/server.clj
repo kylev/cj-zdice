1be335016993d2f18f613ba62d0080989db86caa
Kyle VanderBeek <kylev@kylev.com>
2014-05-12 13:18:58 -0700

    Enable serving public resources from files

--- src/clj/cj_zdice/server.clj
+++ src/clj/cj_zdice/server.clj
@@ -1,13 +1,15 @@
 (ns cj-zdice.server
   (:require [ring.util.response :refer [file-response]]
             [ring.adapter.jetty :refer [run-jetty]]
-            [compojure.core :refer [defroutes GET]]))
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :as route]))
 
 (defn index []
   (file-response "public/html/index.html" {:root "resources"}))
 
 (defroutes routes
-  (GET "/" [] (index)))
+  (GET "/" [] (index))
+  (route/files "/" {:root "resources/public"}))
 
 (def app
   (-> routes))
@@ -15,7 +17,9 @@
 (defonce server
   (run-jetty #'app {:port 8080 :join? false}))
 
-(defn -main [& m]
+
+;; Disabled server stuff for now.
+#_(defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
     (server/start port {:mode mode
