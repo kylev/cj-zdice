d4aa5dab74bac5a37a97bc364c0833fa94246ecc
Kyle VanderBeek <kylev@kylev.com>
2014-05-12 17:47:37 -0700

    Get a basic game view rendering

--- src/cljs/cj_zdice/core.cljs
+++ src/cljs/cj_zdice/core.cljs
@@ -1,1 +1,18 @@
-(ns cj-zdice.core)
+(ns cj-zdice.core
+  (:require [goog.dom :as gdom]
+            [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]))
+
+(enable-console-print!)
+;;(println "Hello world!")
+
+(def app-state
+  (atom {:game {}}))
+
+(defn game-view [app owner]
+  (reify
+    om/IRender
+    (render [_] (dom/div nil "Stuff!"))))
+
+(om/root game-view app-state
+  {:target (gdom/getElement "game")})
