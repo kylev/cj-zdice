4947d3b85010f76789c157dfd58ded7cfdc7433d
Kyle VanderBeek <kylev@kylev.com>
2014-05-17 18:38:55 -0700

    Start breaking out comoponents

--- src/cljs/cj_zdice/core.cljs
+++ src/cljs/cj_zdice/core.cljs
@@ -4,15 +4,27 @@
             [om.dom :as dom :include-macros true]))
 
 (enable-console-print!)
-;;(println "Hello world!")
+(println "Hello world!")
 
 (def app-state
-  (atom {:game {}}))
+  (atom {:user "foo"
+         :scoreboard {}}))
+
+(defn user-view [app owner]
+  (reify
+    om/IRender
+    (render [_] (dom/div #js {:className "user"} (:user app)))))
+
+(defn cup-view [app owner]
+  (reify
+    om/IRender
+    (render [_] (dom/ul nil ""))))
 
 (defn game-view [app owner]
   (reify
     om/IRender
-    (render [_] (dom/div nil "Stuff!"))))
+    (render [_]
+      (dom/div nil (om/build user-view app)))))
 
 (om/root game-view app-state
   {:target (gdom/getElement "game")})
