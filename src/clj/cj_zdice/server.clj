3a60b33684e30e94d2e263515a60bdd647a3fec2
Kyle VanderBeek <kylev@kylev.com>
2014-05-19 18:34:22 -0700

    GET game by id

--- src/clj/cj_zdice/server.clj
+++ src/clj/cj_zdice/server.clj
@@ -15,6 +15,7 @@
 ;; UUID as game identifier.
 (defn uuid [] (str (java.util.UUID/randomUUID)))
 (def uuid-regex #"[a-f\d\-]{36}")
+;; (uuid)
 
 (defn edn-response [data & [status]]
   {:status (or status 200)
@@ -28,14 +29,26 @@
 (defn start-game []
   (edn-response (assoc (game/new) :id (uuid))))
 
-(defn get-game [id]
-  "Thinger")
+;; (def test-game "89230829-9d8f-4dfe-9c46-c93d5620e8f1")
+;; (wcar* (car/set test-game {}))
+;; (wcar* (car/get test-game))
+
+(defn get-game-request
+  "Get a stored game. 404 if it doesn't exist."
+  [id]
+  (let [game (wcar* (car/get id))]
+    (if (nil? game)
+      {:status 404}
+      (edn-response game))))
+
+(defn roll-dice [id])
 
 ;; Routes and the app
 (defroutes routes
   (GET "/" [] (index))
   (POST "/game" [] (start-game))
-  (GET ["/game/:id" :id uuid-regex] [id] (get-game id))
+  (GET ["/game/:id" :id uuid-regex] [id] (get-game-request id))
+  (POST ["/game/:id/roll" :id uuid-regex] [id] (roll-dice id))
   (route/files "/" {:root "resources/public"}))
 
 (def app
@@ -51,3 +64,6 @@
         port (Integer. (get (System/getenv) "PORT" "5000"))]
     (run-jetty #'app {:port port :join? false})))
 
+(defn -heroku
+  "Heroku-specialized server runner."[& m]
+  nil)
