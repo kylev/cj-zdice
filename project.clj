f7f626212d5a2d830cfc7605db7d5b1ee92d02dd
Kyle VanderBeek <kylev@kylev.com>
2014-05-19 17:18:25 -0700

    Roll a start button request with om-sync
    om-sync expects core.asyc to be provided.

--- project.clj
+++ project.clj
@@ -3,8 +3,10 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2173"]
                  [ring/ring "1.2.2"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [com.taoensso/carmine "2.6.2"]
                  [om "0.6.2"]
+                 [om-sync "0.1.1"]
                  [compojure "1.1.6"]
                  [fogus/ring-edn "0.2.0"]]
 
--- src/cljs/cj_zdice/core.cljs
+++ src/cljs/cj_zdice/core.cljs
@@ -1,7 +1,9 @@
 (ns cj-zdice.core
   (:require [goog.dom :as gdom]
             [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [om-sync.core :refer [om-sync]]
+            [om-sync.util :refer [edn-xhr]]))
 
 (enable-console-print!)
 (println "Hello world!")
@@ -11,7 +13,13 @@
          :game-id nil
          :scoreboard {}}))
 
-(def do-roll)
+(defn do-roll [e] (println "Rolling"))
+
+(defn start-game []
+  (edn-xhr
+    {:method :post
+     :url "/game"
+     :on-complete (fn [res] (println res))}))
 
 (defn user-view [app owner]
   (reify
@@ -29,6 +37,13 @@
     (render [_]
       (dom/div nil (om/build user-view app)))))
 
+(defn start-button-view [app owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/button
+        #js {:onClick start-game}
+        "Start"))))
 
 (defn action-buttons-view [app owner]
   (reify
@@ -38,26 +53,15 @@
         (dom/button
            #js {:onClick do-roll}
            "Roll")
-        (dom/button nil "Stop & Score")))))
-
-(defn rolled-die-view [app owner]
-  )
-
-(defn rolled-dice-view [app owner]
-  "The set of rolled dice."
-  (reify
-    om/IRender
-    (render [_]
-      (dom/div #js {:className "dice"}
-        (apply dom/ul nil
-          (map (fn [die-state] "") []))))))
+        (dom/button nil "Stop")))))
 
 (defn game-view [app owner]
   (reify
     om/IRender
     (render [_]
-      (dom/div nil "stuff")
-      (om/build action-buttons-view app))))
+      (dom/div nil
+        (om/build start-button-view app)
+        (om/build action-buttons-view app)))))
 
 
 (om/root game-view app-state
