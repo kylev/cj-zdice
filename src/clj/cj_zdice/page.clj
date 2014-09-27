9803750906c5ce8d4d7946b2353d6cfb270afc1c
Kyle VanderBeek <kylev@kylev.com>
2014-09-27 12:45:51 -0700

    More template fiddling, based on boostrap pages

--- src/clj/cj_zdice/page.clj
+++ src/clj/cj_zdice/page.clj
@@ -1,11 +1,14 @@
 (ns cj-zdice.page
   (:require [net.cgrand.enlive-html :as html]))
 
-;;(html/html-resource "templates/app-script.html")
+(html/defsnippet app-script-debug "templates/app-script-debug.html" [:body] [] html/unwrap)
 
-;;(html/defsnippet app-script-debug "templates/app-script-debug.html" [] [])
+(html/defsnippet app-script "templates/app-script.html" [:body] [] html/unwrap)
 
 (html/deftemplate blah "templates/blah.html"
   [title]
   [:head :title] (html/content title)
-  [:h1] (html/content title))
+  [:h1] (html/content title)
+  [:#app_js] (html/substitute (app-script)))
+
+(html/sniptest app-script-debug)
